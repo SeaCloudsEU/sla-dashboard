0112451ed36ec988c7edd4f3d9fe825b783b2169
Román SG <roman.sosa@atos.net>
2014-12-02 15:36:23 +0000

    bin/runserver now accepts a listening port parameter

--- bin/runserver.sh
+++ bin/runserver.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 #
 # To be executed from project root dir.
+#
+# Usage: $0 [port]
+#   port: listening port; defaults to 8000
+PORT=${1:-8000}
 . ~/virtualenvs/sla-dashboard/bin/activate
-./manage.py runserver 0.0.0.0:8000
+./manage.py runserver 0.0.0.0:$PORT
